808ade56d52ffd5367a556ee75274e9a74f9e765
Ayushman Gupta <ayushmangupta201@gmail.com>
2026-07-08 17:07:15 +0530

    Use Cloudflare API URL

--- frontend/src/api/chat.js
+++ frontend/src/api/chat.js
@@ -1,6 +1,6 @@
 export async function sendMessage(message) {
 
-    const response = await fetch("http://127.0.0.1:8000/chat", {
+    const response = await fetch("https://census-expensive-button-scanned.trycloudflare.com/chat", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
